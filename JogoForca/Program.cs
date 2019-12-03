053d054fe43ce7836f7cc0b7d27bb82b64668bf6
EUDER JOSE DOS SANTOS <euder.jsantos@senacsp.edu.br>
2019-12-03 11:01:12 -0300

    feat: roleta de premios

--- JogoForca/Program.cs
+++ JogoForca/Program.cs
@@ -7,12 +7,17 @@ namespace JogoForca
         static void Main(string[] args)
         {
             // Vetor com as palavras para ser sorteadas.
-            string[] palavras = {"casa", "artista", "desenvolvedor", "futebol", "software", "coca-cola" };
-            
+            string[] palavras = { "casa", "artista", "desenvolvedor", "futebol", "software", "coca-cola" };
+
+            //Vetor com itens da roleta 
+            string[] roleta = { "750", "200", "1000", "950", "600", "Perdeu tudo", "Passou a vez" };
+
 
             // Gerando um número aleatório para sortear a palavra
-            Random random = new Random();          
+            Random random = new Random();
             string palavraSorteada = palavras[random.Next(0, palavras.Length)];
+
+
 
             // Vetor com as letras separadas da palavra sorteada.
             char[] palavraQuebrada = palavraSorteada.ToCharArray();
@@ -28,72 +33,100 @@ namespace JogoForca
             bool acertou = false;
             int qtdeLetras = 0;
             int qtdeAcertos = 0;
+            string itemRoleta = "";
+            double premiototal = 0;
+
 
             // Monta a lista de letras reveladas.
             for (int i = 0; i < palavraQuebrada.Length; i++)
             {
-                if(palavraQuebrada[i].ToString() == "-")
+                if (palavraQuebrada[i].ToString() == "-")
                 {
-                    letrasReveladas[i] = " - ";                    
+                    letrasReveladas[i] = " - ";
                 }
                 else
                 {
-                    letrasReveladas[i] = " _ ";                    
+                    letrasReveladas[i] = " _ ";
                     qtdeLetras++;
                 }
             }
 
-            while(sairJogo == false)
+            while (sairJogo == false)
             {
-             // Mostra para  o usário as info do jogo.
-             Console.Clear();
-             Console.WriteLine("### VOCÊ ESTÁ JOGANDO RODA RODA SENAC DEV ###");
-            Console.WriteLine();
-            Console.WriteLine("Erros: {0} de {1}", qtdErros, limiteErros);
-        Console.WriteLine("A palavra sorteada possui: {0} letras.", qtdeLetras);
+                //Sortear um item da roleta.
+                itemRoleta = roleta[random.Next(0, roleta.Length)];
+                // Mostra para  o usário as info do jogo.
+                Console.Clear();
+                Console.WriteLine("### VOCÊ ESTÁ JOGANDO RODA RODA SENAC DEV ###");
+                Console.WriteLine();
+                Console.WriteLine("Erros: {0} de {1}", qtdErros, limiteErros);
+                Console.WriteLine("A palavra sorteada possui: {0} letras.", qtdeLetras);
+                Console.WriteLine();
+                Console.WriteLine("Valor acumulado;" + premiototal.ToString("C"));
+                Console.WriteLine("Item sorteado pela roleta: " + itemRoleta);
 
-            // Lista as letras reveladas.
-            for (int i = 0; i < letrasReveladas.Length; i++)
-                Console.Write(letrasReveladas[i]);
+                // Lista as letras reveladas.
+                for (int i = 0; i < letrasReveladas.Length; i++)
+                    Console.Write(letrasReveladas[i]);
 
                 //Pergunta ao jogador o palpite de letra
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.Write("Digite uma letra: ");
-                letraEscolhida = Convert.ToChar(Console.ReadLine());
-                
-                // Reiniciar o acerto.
-                acertou = false;
-
-                //Verifica se a letra informada existe na palavra.
-                for (int i = 0; i < letrasReveladas.Length; i++)
+                if (itemRoleta == "Perdeu tudo")
                 {
-                    if(palavraQuebrada[i] == letraEscolhida)
+                    premiototal = 0;
+                    Console.ReadKey();
+                }
+                else if (itemRoleta == "Passou a vez")
+                {
+                    qtdErros++;
+                    Console.ReadKey();
+                }
+                else
+                {
+
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.Write("Digite uma letra: ");
+                    letraEscolhida = Convert.ToChar(Console.ReadLine());
+
+                    // Reiniciar o acerto.
+                    acertou = false;
+
+                    //Verifica se a letra informada existe na palavra.
+                    for (int i = 0; i < letrasReveladas.Length; i++)
                     {
-                        acertou = true;
-                        qtdeAcertos++;
-                        letrasReveladas[i] = letraEscolhida.ToString();
+                        if (palavraQuebrada[i] == letraEscolhida)
+                        {
+                            acertou = true;
+                            qtdeAcertos++;
+                            letrasReveladas[i] = letraEscolhida.ToString();
+                        }
                     }
+                    //Verifica se acertou para acumular o valor sorteado pela roleta.
+
+                    if (acertou == true)
+                    {
+                        premiototal += Convert.ToDouble(itemRoleta);
+                    }
+
+                    //Validando a rodada
+                    if (acertou == false)
+                        qtdErros++;
                 }
 
-                //Validando a rodada
-                if (acertou == false)
-                    qtdErros++;
-
-                if(qtdErros >= limiteErros)
+                if (qtdErros >= limiteErros)
                 {
                     Console.Clear();
                     Console.WriteLine("ERROOOU! QUE PENA, VOCÊ PERDEU. A palavra sorteada era {0}.", palavraSorteada.ToUpper());
                     sairJogo = true;
                 }
 
-                if(qtdeLetras == qtdeAcertos)
+                if (qtdeLetras == qtdeAcertos)
                 {
                     Console.Clear();
                     Console.WriteLine("PARABÉNS, VOCÊ GANHOU! Pois conseguiu descobrir a palavra {0}.", palavraSorteada.ToUpper());
                     sairJogo = true;
                 }
-            }          
+            }
             Console.ReadKey();
         }
     }
